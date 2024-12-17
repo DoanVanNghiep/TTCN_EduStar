d2a3d6a3e0aa732dd2719ef69ec18174de6155e8
DoanVanNghiep <doannghiepnd2k3@gmail.com>
2024-12-17 14:31:49 +0700

    edustar

---
+++ src/main/java/com/example/ttcn2etest/model/etity/Order.java
@@ -0,0 +1,59 @@
+package com.example.ttcn2etest.model.etity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "`order`", schema = "edustar")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "service_manager_id", nullable = false)
+    private Service serviceManager;
+
+    @NotNull
+    @Column(name = "amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "payment_method", nullable = false, length = 50)
+    private String paymentMethod;
+
+    @ColumnDefault("'PENDING'")
+    @Lob
+    @Column(name = "status")
+    private String status;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "payment_date")
+    private Instant paymentDate;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+}
